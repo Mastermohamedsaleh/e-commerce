2433a7f88ca614cf6367d2fda8e17348051652e5
Mohmaed saleh <mohamed@yahoo.com>
2023-03-31 16:39:40 +0200

    Add

--- app/Http/Controllers/Frontend/CheckoutController.php
+++ app/Http/Controllers/Frontend/CheckoutController.php
@@ -5,14 +5,21 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
+
+
 class CheckoutController extends Controller
 {
       
  
     public function checkout(){
-        
          
-          return view("frontend.cart.checkout");
+
+         
+        $items = Cart::where('user_id',Auth::id())->get();
+        
+          return view("frontend.cart.checkout",compact('items'));
     }
       
      
--- resources/views/frontend/cart/checkout.blade.php
+++ resources/views/frontend/cart/checkout.blade.php
@@ -92,40 +92,45 @@ E-Shop
 </div><!--end col -->
 
 <div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
+<label for="">Last Name</label>
+<input type="text" name="last_name" class="form-control" placeholder="LastName">
+</div><!--end col -->
+
+<div class="col-md-6">
+<label for="">Address 1</label>
+<input type="text" name="Address_1" class="form-control" placeholder="Address 1">
 </div><!--end col -->
 <div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
+<label for="">Address 2</label>
+<input type="text" name="Address_2" class="form-control" placeholder="Address 2">
+</div><!--end col -->
+
+
+
+<div class="col-md-6">
+<label for="">Email</label>
+<input type="email" name="email" class="form-control" placeholder="Email">
+</div><!--end col -->
+
+<div class="col-md-6">
+<label for="">Phone Number</label>
+<input type="text" name="phone_number" class="form-control" placeholder="Phone Number">
 </div><!--end col -->
 <div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
+<label for="">City</label>
+<input type="text" name="city" class="form-control" placeholder="City">
 </div><!--end col -->
 <div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
+<label for="">State</label>
+<input type="text" name="state" class="form-control" placeholder="State">
 </div><!--end col -->
 <div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
+<label for="">Country</label>
+<input type="text" name="country" class="form-control" placeholder="Country">
 </div><!--end col -->
 <div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
-</div><!--end col -->
-<div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
-</div><!--end col -->
-<div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
-</div><!--end col -->
-<div class="col-md-6">
-<label for="">Frist Name</label>
-<input type="text" name="first_name" class="form-control" placeholder="FristName">
+<label for="">Pin Code</label>
+<input type="text" name="pin_code" class="form-control" placeholder="Pin Code">
 </div><!--end col -->
 
 
@@ -146,31 +151,76 @@ E-Shop
 
 
  
+
+
+<div class="card shadow p-2">
+
+<h5>Order Details</h5>
+
+<div class="card-body">
+
+
+<table class="text-center table table-striped">
+
+
+ 
+<tr>
+ 
+  <th scope="col">Name</th>
+  <th scope="col">Quantity</th>
+  <th scope="col">Price</th>
+</tr>
+
+
+
+
+ 
+@foreach($items as $item)
+
+<tr>
+
+
+<td>{{$item->product->name}}</td>
+<td>{{$item->quantity}}</td>
+<td>{{$item->product->selling_price}}</td>
+
+
+</tr> 
+
+
+@endforeach
+
+
+
+
+
+
+
+
+
+</table>
+
+
+<hr>
+
+<button class="btn btn-primary  w-100  text-center">Checkout</button>
+
+
+</div> <!--End card-body -->
+
+
+
+
+</div> <!--End card -->
+
+
+
+
   
-   <h5>Order Details</h5>
- 
-    <hr>
-
-    <table class="shadow text-center table table-striped">
-
-
- 
-    <tr>
-     
-      <th scope="col">Name</th>
-      <th scope="col">Quantity</th>
-      <th scope="col">Price</th>
-    </tr>
-  
-
-
-
 
  
 
-
-<button class="bisplay-block">sss</button>
-    </table>
+ 
 
 
 
